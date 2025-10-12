1c5e887b56740e12c9a8e1915b0550df7f0aa0b7
Sander Vreeken <info@sandervreeken.com>
2025-10-12 09:03:29 +0200

    Installed polar.sh

--- apps/api/src/lib/auth.ts
+++ apps/api/src/lib/auth.ts
@@ -1,7 +1,18 @@
+import { checkout, polar, portal, usage } from '@polar-sh/better-auth'
+import { Polar } from '@polar-sh/sdk'
 import { betterAuth } from 'better-auth'
 import { drizzleAdapter } from 'better-auth/adapters/drizzle'
 
 import { db } from '@/db/index'
+import env from '@/env'
+
+const polarClient = new Polar({
+  accessToken: env.POLAR_ACCESS_TOKEN,
+  // Use 'sandbox' if you're using the Polar Sandbox environment
+  // Remember that access tokens, products, etc. are completely separated between environments.
+  // Access tokens obtained in Production are for instance not usable in the Sandbox environment.
+  server: 'sandbox',
+})
 
 export const auth = betterAuth({
   emailAndPassword: {
@@ -10,5 +21,25 @@ export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: 'pg',
   }),
+  plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: '5d085005-3936-4416-9aaf-03a9aea3a8d5',
+              slug: 'premium',
+            },
+          ],
+          successUrl: env.POLAR_SUCCESS_URL,
+          authenticatedUsersOnly: true,
+        }),
+        portal(),
+        usage(),
+      ],
+    }),
+  ],
   trustedOrigins: ['http://localhost:3000', 'http://localhost:5173'],
 })
