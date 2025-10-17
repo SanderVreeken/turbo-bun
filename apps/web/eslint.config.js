65402edd77a280e660b696153231b0618165c031
Sander Vreeken <info@sandervreeken.com>
2025-10-17 13:15:32 +0200

    Added sidebar in the /application route

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -4,7 +4,7 @@ import { config } from '@repo/eslint-config/svelte';
 
 export default [
 	...config,
-	{ ignores: ['.svelte-kit/*', 'build/*'] },
+	{ ignores: ['.svelte-kit/*', 'build/*', 'src/lib/components/ui/*', 'src/lib/hooks/*'] },
 	prettier,
 	...svelte.configs.prettier
 ];
