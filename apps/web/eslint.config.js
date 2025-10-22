c6c7f64bfbbc1dede30a12f7e578c6af19a20590
Sander <sander@Sanders-MacBook-Pro.local>
2025-10-22 17:18:32 +0200

    Updated eslint config

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,10 +1,8 @@
 import prettier from 'eslint-config-prettier';
-import svelte from 'eslint-plugin-svelte';
 import { config } from '@repo/eslint-config/svelte';
 
 export default [
 	...config,
 	{ ignores: ['.svelte-kit/*', 'build/*', 'src/lib/components/ui/*', 'src/lib/hooks/*'] },
-	prettier,
-	...svelte.configs.prettier
+	prettier
 ];
